3914b44911a91ec45b521012fcea83c21dc539a3
Nasrul <122144023+Nasrul78@users.noreply.github.com>
2025-09-12 12:59:54 +0700

    Aesthetic changes

--- server/server.js
+++ server/server.js
@@ -1,81 +1,81 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const cors = require("cors")
 
-const app = express();
-const server = http.createServer(app);
+const app = express()
+const server = http.createServer(app)
 
 app.use(
   cors({
-    origin: '*',
+    origin: "*",
   })
-);
+)
 
 const io = new Server(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
+    origin: "*",
+    methods: ["GET", "POST"],
   },
-});
+})
 
-app.get('/', (req, res) => {
-  res.send('Server is running!');
-});
+app.get("/", (req, res) => {
+  res.send("Server is running!")
+})
 
-let rooms = {};
+let rooms = {}
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   // Connected
-  console.log(`User connected ${socket.id}`);
+  console.log(`User connected ${socket.id}`)
 
   // Create room
-  socket.on('create-room', ({ roomId, name }) => {
+  socket.on("create-room", ({ roomId, name }) => {
     if (rooms[roomId]) {
-      socket.emit('create-error', `Room ${roomId} already exists`);
+      socket.emit("create-error", `Room ${roomId} already exists`)
     } else {
-      const player = { id: socket.id, name, role: 'operator' };
-      rooms[roomId] = [player];
+      const player = { id: socket.id, name, role: "operator" }
+      rooms[roomId] = [player]
 
-      socket.join(roomId);
-      socket.roomId = roomId;
-      socket.emit('room-created', player);
+      socket.join(roomId)
+      socket.roomId = roomId
+      socket.emit("room-created", player)
 
-      console.log('Room created:', rooms);
+      console.log("Room created:", rooms)
     }
-  });
+  })
 
   // Join Room
-  socket.on('join-room', ({ roomId, name }) => {
+  socket.on("join-room", ({ roomId, name }) => {
     if (!rooms[roomId]) {
-      socket.emit('join-error', `Room not found: ${roomId}`);
+      socket.emit("join-error", `Room not found: ${roomId}`)
     } else if (rooms[roomId].some((p) => p.id === socket.id)) {
-      socket.emit('room-joined', roomId);
+      socket.emit("room-joined", roomId)
     } else {
-      const player = { id: socket.id, name, role: 'member' };
-      rooms[roomId].push(player);
+      const player = { id: socket.id, name, role: "member" }
+      rooms[roomId].push(player)
 
-      socket.join(roomId);
-      socket.roomId = roomId;
-      socket.emit('room-joined', roomId);
-      io.to(roomId).emit('player-joined', player);
+      socket.join(roomId)
+      socket.roomId = roomId
+      socket.emit("room-joined", roomId)
+      io.to(roomId).emit("player-joined", player)
 
-      console.log(rooms);
+      console.log(rooms)
     }
-  });
+  })
 
   // Message
-  socket.on('send-message', ({ msg, name }) => {
-    io.to(socket.roomId).emit('receive-message', `${name}: ${msg}`);
-  });
+  socket.on("send-message", ({ msg, name }) => {
+    io.to(socket.roomId).emit("receive-message", `${name}: ${msg}`)
+  })
 
   // Disconnected
-  socket.on('disconnect', () => {
-    console.log(`User disconnected ${socket.id}`);
-  });
-});
+  socket.on("disconnect", () => {
+    console.log(`User disconnected ${socket.id}`)
+  })
+})
 
-const PORT = 3000;
-server.listen(PORT, '0.0.0.0', () => {
-  console.log('listening to port 3000');
-});
+const PORT = 3000
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("listening to port 3000")
+})
